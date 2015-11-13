08dc4580d18fba8bed52b5ea4b47c8ea80906814
Ghelo Tolentino <ghelo@johngaltventures.com>
2015-11-13 10:40:26 +0800

    * Spaced out classes per PEP

--- chikka/exceptions.py
+++ chikka/exceptions.py
@@ -1,18 +1,22 @@
-
 class NullMobileNumberException(Exception):
     pass
+
 
 class InvalidMobileNumberException(Exception):
     pass
 
+
 class NullClientIDException(Exception):
     pass
+
 
 class NullSecretKeyException(Exception):
     pass
 
+
 class NullShortCodeException(Exception):
     pass
+
 
 class NullRequestCostException(Exception):
     pass
