82ea10fef0c0e92af3d55a9093bee36c7c64c368
Ghelo Tolentino <ghelo@johngaltventures.com>
2016-03-28 19:13:10 +0800

    Incremented version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="python-chikka",
-    version="0.5.1",
+    version="0.5.2",
     description="Python API Wrapper for Chikka ",
     author="Mark Allan B Meriales",
     author_email="mark.meriales@gmail.com",
